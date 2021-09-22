6b01f0ece67b652e641c50944f3f37297673771c
zeng <58822089+15297718239@users.noreply.github.com>
2021-09-22 21:19:52 +0800

    8-10 增加保存文档内容

---
+++ src/main/java/com/jiawa/wiki/req/DocQueryReq.java
@@ -0,0 +1,9 @@
+package com.jiawa.wiki.req;
+
+public class DocQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DocQueryReq{} " + super.toString();
+    }
+}
---
+++ src/main/java/com/jiawa/wiki/service/DocService.java
@@ -0,0 +1,109 @@
+package com.jiawa.wiki.service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.jiawa.wiki.domain.Content;
+import com.jiawa.wiki.domain.Doc;
+import com.jiawa.wiki.domain.DocExample;
+import com.jiawa.wiki.mapper.ContentMapper;
+import com.jiawa.wiki.mapper.DocMapper;
+import com.jiawa.wiki.req.DocQueryReq;
+import com.jiawa.wiki.req.DocSaveReq;
+import com.jiawa.wiki.resp.DocQueryResp;
+import com.jiawa.wiki.resp.PageResp;
+import com.jiawa.wiki.utils.CopyUtil;
+import com.jiawa.wiki.utils.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * @Description TODO
+ * @Author zengjinming
+ * @Date 2021/9/17 14:51
+ */
+@Service
+public class DocService {
+    private static final Logger LOG = LoggerFactory.getLogger(DocService.class);
+
+    @Resource
+    private DocMapper docMapper;
+    @Resource
+    private ContentMapper contentMapper;
+    @Resource
+    private SnowFlake snowFlake;
+
+    public List<DocQueryResp> all() {
+        DocExample docExample = new DocExample();
+        docExample.setOrderByClause("sort asc");
+        List<Doc> docList = docMapper.selectByExample(docExample);
+        //列表复制
+        List<DocQueryResp> list = CopyUtil.copyList(docList, DocQueryResp.class);
+        return list;
+    }
+    public PageResp<DocQueryResp> list(DocQueryReq req) {
+
+        DocExample docExample = new DocExample();
+        docExample.setOrderByClause("sort asc");
+        DocExample.Criteria criteria = docExample.createCriteria();
+
+        PageHelper.startPage(req.getPage(),req.getSize());
+        List<Doc> docList = docMapper.selectByExample(docExample);
+
+        PageInfo<Doc> pageInfo = new PageInfo<>(docList);
+        LOG.info("总行数:{}",pageInfo.getTotal());
+        LOG.info("总页数:{}",pageInfo.getPages());
+
+//        List<DocResp> respList = new ArrayList<>();
+//        for (Doc doc:docList) {
+////            DocResp docResp = new DocResp();
+////            BeanUtils.copyProperties(doc,docResp);//工具类把结果集类doclist 复制到DocResp类型中；
+//            DocResp docResp = CopyUtil.copy(doc, DocResp.class);
+//            respList.add(docResp);
+//        }
+        //列表复制
+
+        List<DocQueryResp> list = CopyUtil.copyList(docList, DocQueryResp.class);
+        PageResp<DocQueryResp> pageResp = new PageResp<>();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
+    }
+    /*
+    保存
+     */
+    public void save(DocSaveReq req){
+        Doc doc = CopyUtil.copy(req, Doc.class);
+        Content content = CopyUtil.copy(req, Content.class);
+        if (ObjectUtils.isEmpty(req.getId())){
+            //新增
+            doc.setId( snowFlake.nextId());
+            docMapper.insert(doc);
+            content.setId( doc.getId());
+            contentMapper.insert(content);
+        }else {
+            //更新
+            docMapper.updateByPrimaryKey(doc);
+            int count=contentMapper.updateByPrimaryKeyWithBLOBs(content);
+            if (count==0){
+                contentMapper.insert(content);
+            }
+        }
+    }
+
+    public void delete(Long id){
+        docMapper.deleteByPrimaryKey(id);
+    }
+    public void delete(List<String> ids){
+        DocExample docExample = new DocExample();
+        DocExample.Criteria criteria = docExample.createCriteria();
+        criteria.andIdIn(ids);
+        docMapper.deleteByExample(docExample);
+    }
+
+
+}
